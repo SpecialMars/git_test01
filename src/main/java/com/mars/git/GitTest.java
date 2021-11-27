ca280e43534c1b83719d5809cc910bb775122436
mars <876679241@qq.com>
2021-11-27 20:40:55 +0800

    marget commit1

--- src/main/java/com/mars/git/GitTest.java
+++ src/main/java/com/mars/git/GitTest.java
@@ -15,5 +15,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hot-fix commit1");
+        System.out.println("hot-fix commit2");
+        System.out.println("marget commit1");
     }
 }
